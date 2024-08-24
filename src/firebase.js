04616435da1518a03e1c8632cd59ade2fb6aac13
Hector Isaac Ortiz Rosas <hiortiz0@gmail.com>
2024-08-24 17:31:18 -0600

    refactor: Update .gitignore and package.json, add dotenv dependency

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
 import { initializeApp } from "firebase/app";
 
-import serviceAccount from "../firebase.json" assert { type: "json" };
+import serviceAccount from "../serviceAccount.json" assert { type: "json" };
 
 const appfire = initializeApp({
   apiKey: process.env.APIKEY,
